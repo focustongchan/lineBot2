76025ac86e51e1816cc87c7c078003feaa7baafd
focustongchan <52289237+focustongchan@users.noreply.github.com>
2019-07-19 10:55:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,15 @@ def callback():
     decoded = json.loads(json_line)
     user = decoded["events"][0]['replyToken']
     userText = decoded["events"][0]['message']['text']
-    sendText(user,userText)
+    #sendText(user,userText)
+    userText = input('." "')
+    if (userText == 'สวัสดี') :
+        sendText(user,'เออว่ะ')
+    elif (userText == 'ไม่ล่ะ') :
+       sendText(user,'ไปไหนก็ไป')
+    else :
+        sendText(user,'ว่าไงน่ะ')
+ 
     return '',200
 
 def sendText(user, text):
